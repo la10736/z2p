eaa8c16b6a97b26cf113f1c3d412385de66f8925
Michele d'Amico <michele.damico@gmail.com>
2020-10-08 14:10:58 +0200

    Getting start with tide

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,6 +1,21 @@
 use z2p::hello;
 
+use tide::Request;
+
 #[cfg(not(tarpaulin_include))]
-fn main() {
-    println!("{}", hello());
+#[async_std::main]
+async fn main() -> tide::Result<()> {
+    let mut app = tide::new();
+    app.at("/").get(greet);
+    app.at("/:name").get(greet);
+    app.listen("127.0.0.1:8080").await?;
+    Ok(())
+}
+
+async fn greet(req: Request<()>) -> tide::Result {
+    let message = req
+        .param::<String>("name")
+        .map(|name| format!("Hello, {}!", name))
+        .unwrap_or(hello().to_owned());
+    Ok(message.into())
 }
